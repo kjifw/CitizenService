c22c44722f5be7a8866c873dbbde615d205e95f1
kjifw <kjifw@kjifw.kjifw>
2016-01-09 16:16:31 +0200

    - added sign in and register fragments - updated main activity - added simple initial navigation

--- Client/CitizenService/app/src/main/java/com/citizen/service/citizenservice/MainActivity.java
+++ Client/CitizenService/app/src/main/java/com/citizen/service/citizenservice/MainActivity.java
@@ -1,13 +1,65 @@
 package com.citizen.service.citizenservice;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.mainViewPager);
+        MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+    }
+
+    public void navigateToRegister(View view) {
+        viewPager.setCurrentItem(1);
+    }
+
+    public void onSignInButtonClick(View view) {
+        EditText email = (EditText) findViewById(R.id.signInEmail);
+        EditText password = (EditText) findViewById(R.id.signInPassword);
+
+    }
+
+    public void onRegisterButtonClick(View view) {
+        EditText email = (EditText) findViewById(R.id.registerEmail);
+        EditText password = (EditText) findViewById(R.id.registerPassword);
+        EditText confirmPassword = (EditText) findViewById(R.id.registerRepeatPassword);
+
+     }
+
+    public class MainPagerAdapter extends FragmentPagerAdapter {
+        public MainPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new SignInFragment();
+                case 1:
+                    return new RegisterFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
---
+++ Client/CitizenService/app/src/main/java/com/citizen/service/citizenservice/RegisterFragment.java
@@ -0,0 +1,16 @@
+package com.citizen.service.citizenservice;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return  view;
+    }
+}
